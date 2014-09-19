1ab11ad55e004bbaf702b615dc651b92f559d49d
karn09 <karn09@gmail.com>
2014-09-19 18:47:44 -0400

    added service

--- finance2.js
+++ finance2.js
@@ -2,17 +2,16 @@ angular.module('finance2', [])
   .factory('currencyConverter', function() {
     var currencies = ['USD', 'EUR', 'CNY'];
     var usdToForeignRates = {
-      USD: 1;
-      EUR: 0.74;
-      CNY: 6.09;
+      USD: 1,
+      EUR: 0.74,
+      CNY: 6.09
+    };
+    var convert = function (amount, inCurr, outCurr) {
+      return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
     };
 
-    var convert = function (amount, inCurr, outCurr) {
-      return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr]
-    }
-
     return {
-      currencies: currencies;
-      convert: convert;
+      currencies: currencies,
+      convert: convert
     };
   });
--- invoice2.js
+++ invoice2.js
@@ -1,18 +1,18 @@
 angular.module('invoice2', ['finance2'])
-  // .controller('CONTROLLER_NAME', ['DEPENDENCY_NAME', function(OBJ FROM DEP) { CODE }])
-  .controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
+// .controller('CONTROLLER_NAME', ['DEPENDENCY_NAME_INJECTOR', function(OBJ FROM DEP) { CODE }])
+.controller('InvoiceController', ['currencyConverter',
+  function(currencyConverter) {
     this.qty = 1;
     this.cost = 2;
-    this.inCurr = 'EUR';;
+    this.inCurr = 'EUR';
     this.currencies = currencyConverter.currencies;
 
-    this.total = function total (outCurr) {
+    this.total = function total(outCurr) {
       // call .convert method decl in dep, pass args: amount, incurr, outcurr
-      return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr)
+      return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
     };
-
-    this.pay = function pay () {
-      window.alert("Thanks");
+    this.pay = function pay() {
+      window.alert("Thanks!");
     };
-    
-  }]);
+  }
+]);
